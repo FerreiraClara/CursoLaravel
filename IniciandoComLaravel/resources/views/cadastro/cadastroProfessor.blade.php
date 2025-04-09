0f4560c4e49c06d2851a11bdbc5535fca9b9ddee
Clara Rocha Ferreira <clararf.ferreira@gmail.com>
2025-04-09 11:35:42 -0300

    Desenvolvimento do cadastro professor | Primeiro teste usando estrutura condicional blade

--- IniciandoComLaravel/resources/views/cadastro/cadastroProfessor.blade.php
+++ IniciandoComLaravel/resources/views/cadastro/cadastroProfessor.blade.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>View Cadastro Professor</h1>
-    
-</body>
-</html>
+@extends('layout.index')
+@section('conteudo')
+
+<div class="form-container">
+    <h2>Cadastro de Professor</h2>
+    <form action="/professores" method="POST">
+      <!-- CSRF para Laravel -->
+      <!-- @csrf -->
+
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome" required>
+
+      <label for="telefone">Telefone:</label>
+      <input type="text" id="telefone" name="telefone" placeholder="(xx) xxxxx-xxxx" required>
+
+      <label for="turma">Turma Responsável:</label>
+      <input type="text" id="turma" name="turma" required>
+
+      <button type="submit">Cadastrar Professor</button>
+    </form>
+  </div>
+
+
+    <h1>Listagem professores</h1>
+    @if(2 == 1)
+        <p>É igual</p>
+    @elseif(2 > 1)
+        <p>É maior</p>
+    @endif
+
+@endsection
